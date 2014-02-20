87f07394a5a9a63cccbeb61aaada10d05f562f6f
Marcello Nicoletti <happydude11209@gmail.com>
2014-02-19 16:59:18 -0800

    Some progress

--- src/main.c
+++ src/main.c
@@ -1,21 +1,18 @@
 #include <pebble.h>
 
-Window *my_window;
-TextLayer *text_layer;
+Window *main_window;
 
 void handle_init(void) {
-	  my_window = window_create();
+	main_window = window_create();
 
-	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
 }
 
 void handle_deinit(void) {
-	  text_layer_destroy(text_layer);
-	  window_destroy(my_window);
+	window_destroy(main_window);
 }
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+	handle_init();
+	app_event_loop();
+	handle_deinit();
 }
